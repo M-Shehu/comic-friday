a3cb5733e173f27d793a3ec4284420cdd00e6e4a
Muhammad Shehu <magnanimus.shehu@gmail.com>
2021-02-12 01:29:25 +0100

    feat: Add twitter link to footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,4 +1,4 @@
-import { HStack, Text } from "@chakra-ui/react";
+import { HStack, Link, Text } from "@chakra-ui/react";
 
 function Footer() {
   return (
@@ -7,7 +7,14 @@ function Footer() {
         Comic Fridays App
       </Text>
       <Text fontSize={["10px", "xs"]} fontWeight="300">
-        Meticulously crafted by Shehu
+        Meticulously crafted by{" "}
+        <Link
+          href="https://twitter.com/DevShehu"
+          isExternal
+          textDecoration="underline"
+        >
+          Shehu
+        </Link>
       </Text>
       <Text fontSize={["10px", "xs"]} fontWeight="300">
         Per il Mio 🍍
